9f9f016a33c878ac769e2be7784b63e645d795f1
令狐少侠 <xxx@yy.com>
2022-12-17 05:29:09 +0800

    feat: 注入Restaurant Respsitory和Restaurant Service

--- src/restaurant/restaurant.module.ts
+++ src/restaurant/restaurant.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantResolver } from './restaurant.resolver';
+import { RestaurantService } from './restaurant.service';
 
 @Module({
-  providers: [RestaurantResolver],
+  imports: [TypeOrmModule.forFeature([Restaurant])],
+  providers: [RestaurantResolver, RestaurantService],
 })
 export class RestaurantModule {}
--- src/restaurant/restaurant.resolver.ts
+++ src/restaurant/restaurant.resolver.ts
@@ -1,9 +1,17 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './entities/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurant.service';
 
 @Resolver()
 export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+
+  @Query(() => [Restaurant])
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
+  }
+
   @Query(() => [Restaurant])
   myRestaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     return [];
