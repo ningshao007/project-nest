5592ebc8a448570a30ea83aac5af978f2e444b02
令狐少侠 <xxx@yy.com>
2022-12-17 04:21:15 +0800

    feat: Args

--- src/restaurant/restaurant.resolver.ts
+++ src/restaurant/restaurant.resolver.ts
@@ -1,10 +1,10 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver()
 export class RestaurantResolver {
-  @Query(() => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query(() => [Restaurant])
+  myRestaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
