af395d70b4157724c961ebea329aa02dae1f414e
令狐少侠 <xxx@yy.com>
2022-12-17 04:54:37 +0800

    feat: 引入ConfigModule配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,6 +7,11 @@ import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: process.env.NODE_ENV !== 'production',
