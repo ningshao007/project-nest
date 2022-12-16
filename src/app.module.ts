663f85ad8e7839f39e67214e713d19da8a94ce68
令狐少侠 <xxx@yy.com>
2022-12-17 04:44:10 +0800

    feat: 连接postgres数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
@@ -9,6 +10,16 @@ import { RestaurantModule } from './restaurant/restaurant.module';
       driver: ApolloDriver,
       playground: process.env.NODE_ENV !== 'production',
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5431,
+      username: 'ningshao',
+      password: '12345',
+      database: 'ningshao1',
+      synchronize: true,
+      logging: true,
     }),
     RestaurantModule,
   ],
