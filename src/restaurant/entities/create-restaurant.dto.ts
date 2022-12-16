27a8069126f359d7a2d5b577401765826fdb8f88
令狐少侠 <xxx@yy.com>
2022-12-17 04:36:27 +0800

    使用参数校验

--- src/restaurant/entities/create-restaurant.dto.ts
+++ src/restaurant/entities/create-restaurant.dto.ts
@@ -1,16 +1,22 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsString, IsBoolean, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(() => String)
+  @IsString()
+  @Length(2, 10)
   name: string;
 
   @Field(() => Boolean)
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(() => String)
+  @IsString()
   address: string;
 
   @Field(() => String)
+  @IsString()
   ownerName: string;
 }
