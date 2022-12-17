76ae87b4214e0cc54f122a50dc863aa894859491
令狐少侠 <xxx@yy.com>
2022-12-18 00:47:05 +0800

    feat: 邮箱认证完成后删除相应信息

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -152,7 +152,8 @@ export class UserService {
 
       if (verification) {
         verification.user.verified = true;
-        this.users.save(verification.user);
+        await this.users.save(verification.user);
+        await this.verifications.delete(verification.id);
 
         return {
           ok: true,
