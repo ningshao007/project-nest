0a497c44fed3d245f330cdb3774aa4d1e0801084
令狐少侠 <xxx@yy.com>
2022-12-17 14:47:44 +0800

    chore: 添加注释

--- src/common/entities/core.entity.ts
+++ src/common/entities/core.entity.ts
@@ -1,3 +1,4 @@
+import { Field } from '@nestjs/graphql';
 import {
   CreateDateColumn,
   PrimaryGeneratedColumn,
@@ -6,11 +7,15 @@ import {
 
 export class CoreEntity {
   @PrimaryGeneratedColumn()
+  // NOTE: 这个@Field装饰器是用来将id字段映射到GraphQL中去的,在GraphQL查询中使用,如果去掉了,就不能在GraphQL查询中使用了
+  @Field(() => Number)
   id: number;
 
   @CreateDateColumn()
+  @Field(() => Date)
   createAt: Date;
 
   @UpdateDateColumn()
+  @Field(() => Date)
   updateAt: Date;
 }
