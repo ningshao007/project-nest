d9a7c3ac58f0e5339d47477c55555e0cc8c6b75d
令狐少侠 <xxx@yy.com>
2022-12-18 16:07:14 +0800

    refactor: AuthGuard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,16 +1,46 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { JwtService } from 'src/jwt/jwt.service';
+import { UserService } from 'src/user/user.service';
+import { AllowRole } from './role.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {
-    const gqlContext = GqlExecutionContext.create(context).getContext();
-    const user = gqlContext['user'];
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
 
-    if (!user) {
-      return false;
+  async canActivate(context: ExecutionContext) {
+    const roles = this.reflector.get<AllowRole>('roles', context.getHandler());
+
+    if (!roles) {
+      return true;
     }
 
-    return true;
+    const gqlContext = GqlExecutionContext.create(context).getContext();
+    const token = gqlContext.token;
+
+    if (token) {
+      const decoded = this.jwtService.verify(token.toString());
+
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        const { user } = await this.userService.findById(decoded['id']);
+
+        if (user) {
+          gqlContext['user'] = user;
+
+          if (roles.includes('Any')) {
+            return true;
+          }
+
+          return roles.includes(user.role);
+        }
+      }
+    }
+
+    return false;
   }
 }
